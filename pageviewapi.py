80b6ed7aaeb056ccebebb6ee3f82c2867842e4fd
ashermanwmf <shermanandrew50@gmail.com>
2016-02-23 11:49:39 -0500

    general purpose solution. can be used to get page views for all meta pages and subpages (still working on translated subpages.

--- pageviewapi.py
+++ pageviewapi.py
@@ -4,18 +4,20 @@ import datetime
 from collections import defaultdict
 from mwviews.api import PageviewsClient
 
+today = datetime.datetime.today() 
+
 p = PageviewsClient()
 
 # the first blank spot is reserved for the english version or 'original article page'. Added langauge codes after.
-code = [ '', 'cs']
+code = [ '' ]
 
 for c in code:
 
 	t = sys.argv[1]
 
-	sd = sys.argv[2]
+	sd = '20150901'
 
-	ed = sys.argv[3]
+	ed = today.strftime('%Y%m%d') 
 
 	l = '%2f'
 
@@ -24,20 +26,17 @@ for c in code:
 	else:
 		l = l + c
 
-	try: 
-		views = p.article_views('meta.wikimedia.org', [t + l], access='all-access', start=sd , end=ed )
+	views = p.article_views('meta.wikimedia.org', [t + l], access='all-access', start=sd , end=ed )
 
-		a = int()
+	a = int()
 
-		for key, value in views.iteritems():
+	for key, value in views.iteritems():
 
-			v = value.get(t)
+		v = value.get(t)
 
-			if isinstance(v, int) == True:
-				a += v
-			else:
-				pass
-	except Exception:
-		a = 'N/A'
+		if isinstance(v, int) == True:
+			a += v
+		else:
+			pass
 
 	print t + '/' + c + ' ' + str(a)
