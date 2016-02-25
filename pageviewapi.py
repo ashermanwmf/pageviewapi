92138886e713f184f7cfe6e2e98ea65bf05c5397
ashermanwmf <shermanandrew50@gmail.com>
2016-02-25 11:13:41 -0500

    fixed spacing

--- pageviewapi.py
+++ pageviewapi.py
@@ -1,10 +1,10 @@
 #########################################################################
-#							How this works								#
-# 1. This script accepts two arguments.							  		#
-# 2. Argument one is the wiki project <wiki.wiki(m/p)edia>.  			#
-# 3. Arguement two is the name of the article or page.		 			#
-# 4. turn graphing on by removing the # in front of drawgraph(graph).	#
-# ex: $ python pageviewapi.py commons.wikimedia File:Dog.png	 		#
+# 							How this works								
+# 1. This script accepts two arguments.							  		
+# 2. Argument one is the wiki project <wiki.wiki(m/p)edia>.  			
+# 3. Arguement two is the name of the article or page.		 			
+# 4. turn graphing on by removing the # in front of drawgraph(graph).	
+# ex: $ python pageviewapi.py commons.wikimedia File:Dog.png
 #########################################################################
 
 import os
